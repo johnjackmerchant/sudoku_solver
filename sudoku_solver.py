a62360a1ddd708b984a23cef63d1ef13b2a9bca8
johnjackmerchant <johnmerchant2006@gmail.com>
2026-09-10 19:40:35 -0400

    Add box validation and combined move check

--- sudoku_solver.py
+++ sudoku_solver.py
@@ -20,6 +20,36 @@ def is_column_valid(board, column_index, number):
             return False
     return True
 
+def is_box_valid(board, row_index, column_index, number):
+    if row_index < 3:
+        row_start = 0
+    elif row_index < 6:
+        row_start = 3
+    else:
+        row_start = 6
+
+    if column_index < 3:
+        column_start = 0
+    elif column_index < 6:
+        column_start = 3
+    else:
+        column_start = 6
+
+    for row in range(row_start, row_start + 3):
+        for column in range(column_start, column_start + 3):
+            if board[row][column] == number:
+                return False
+
+    return True
+
+def is_valid(board, row_index, column_index, number):
+    if is_row_valid(board, row_index, number) and is_column_valid(board, column_index, number) and is_box_valid(board, row_index, column_index, number):
+        return True
+    else:
+        return False
+
+
+
 board = [
     [0,0,0,0,0,0,0,0,0],
     [0,0,0,0,0,0,0,0,0],
@@ -32,5 +62,5 @@ board = [
     [0,0,0,0,0,0,0,0,0],
 ]
 
-print(is_column_valid(board, 2, 7))
+
 
