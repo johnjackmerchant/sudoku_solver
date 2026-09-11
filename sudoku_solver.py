4f4c94a4f39eabae4334cc86fe43f06e1e949399
johnjackmerchant <johnmerchant2006@gmail.com>
2026-09-11 15:08:00 -0400

    Connect board input, validation, and solving

--- sudoku_solver.py
+++ sudoku_solver.py
@@ -1,29 +1,30 @@
+#prints board
 def print_board(board):
     for i in range(len(board)):
         print(board[i])
 
-
+#finds empty cells
 def find_empty(board):
     for row_index in range(len(board)):
         for column_index in range(len(board[row_index])):
             if board[row_index][column_index] == 0:
                 return row_index, column_index
 
-
+#determines if number can go in row
 def is_row_valid(board, row_index, number):
     for cell in board[row_index]:
         if cell == number:
             return False
     return True
 
-
+#determines if number can go in column
 def is_column_valid(board, column_index, number):
     for row_index in range(len(board)):
         if board[row_index][column_index] == number:
             return False
     return True
 
-
+#determines if number can go in box
 def is_box_valid(board, row_index, column_index, number):
     if row_index < 3:
         row_start = 0
@@ -46,14 +47,14 @@ def is_box_valid(board, row_index, column_index, number):
 
     return True
 
-
+#determines if number can go in a cell
 def is_valid(board, row_index, column_index, number):
     if is_row_valid(board, row_index, number) and is_column_valid(board, column_index, number) and is_box_valid(board, row_index, column_index, number):
         return True
     else:
         return False
 
-
+#solves board by finding empty cell and calling is_valid
 def solve(board):
     empty_position = find_empty(board)
     if empty_position is None:
@@ -71,21 +72,54 @@ def solve(board):
 
     return False 
 
+#determines if entered board has conflicting clues
+def is_board_valid(board):
+    for row_index in range(len(board)):
+        for column_index in range(len(board[row_index])):
+            if board[row_index][column_index] == 0:
+                continue
+            else:
+                temp = board[row_index][column_index]
+                board[row_index][column_index] = 0
+                cell_check = is_valid(board, row_index, column_index, temp)
+                board[row_index][column_index] = temp
+                if cell_check == True:
+                    continue
+                else:
+                    return False
 
+    return True
 
-board = [
-    [5,3,0,0,7,0,0,0,0],
-    [6,0,0,1,9,5,0,0,0],
-    [0,9,8,0,0,0,0,6,0],
-    [8,0,0,0,6,0,0,0,3],
-    [4,0,0,8,0,3,0,0,1],
-    [7,0,0,0,2,0,0,0,6],
-    [0,6,0,0,0,0,2,8,0],
-    [0,0,0,4,1,9,0,0,5],
-    [0,0,0,0,8,0,0,7,9],
-]
+#user instructions
+print("Enter 9 digits per row, using 0 for empty cells")
 
-solve(board)
+#gets rows from user and validates rows
+entered_rows = []
+for row in range(1,10):
+    i = 0
+    while i != 1:
+        user_row = (input(f"Enter row {str(row)}: "))
+        if len(user_row) == 9:
+            valid_digits = True
+            for num in user_row:
+                if not num in ("0123456789"):
+                    valid_digits = False
+                    print("Each row may only contain digits 0-9")
+                    break 
+        else:
+            print("Each row may only contain 9 digits")
+            continue        
+        if valid_digits == True:
+            clean_row = [int(num) for num in user_row]
+            entered_rows.append(clean_row)
+            i += 1
 
-print_board(board)
-
+#solves board if valid       
+if is_board_valid(entered_rows):
+    if solve(entered_rows):
+        print("Puzzle solved!")
+        print_board(entered_rows)
+    else:
+        print("No solution found")
+else:
+    print("Entered board has conflicting clues")
